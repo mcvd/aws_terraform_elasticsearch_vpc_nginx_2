9f41346bc7738ef39fb796d6566c02f81cd23b79
Danilo Desole <danilodesole@gmail.com>
2020-01-20 17:15:02 +0000

    wip

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,10 +1,10 @@
-# data "template_file" "bootstrap" {
-#   template = file("${path.module}/bootstrap2.sh")
+data "template_file" "bootstrap" {
+  template = file("${path.module}/bootstrap2.sh")
 
-#   vars = {
-#     es_cluster_address = var.es_cluster_address
-#   }
-# }
+  vars = {
+    es_cluster_address = var.es_cluster_address
+  }
+}
 
 data "aws_iam_instance_profile" "default" {
   name = var.iam_profile_name
@@ -19,7 +19,7 @@ resource "aws_instance" "default" {
   associate_public_ip_address = false
   iam_instance_profile = join("", data.aws_iam_instance_profile.default.*.name)
 
-  # user_data = data.template_file.bootstrap.rendered
+  user_data = data.template_file.bootstrap.rendered
 
   root_block_device {
     volume_type           = "gp2"
@@ -28,12 +28,12 @@ resource "aws_instance" "default" {
     kms_key_id = var.kms_key_id
   }
 
-  provisioner "remote-exec" {
-    inline =  [
-      "echo ${var.instance_name} > /usr/share/nginx/html/index.html",
-      "systemctl restart nginx"
-    ]
-  }
+  # provisioner "remote-exec" {
+  #   inline =  [
+  #     "echo ${var.instance_name} > /usr/share/nginx/html/index.html",
+  #     "systemctl restart nginx"
+  #   ]
+  # }
 
   tags = {
     Environment = "TFTest"
