2077a8fed6e3361eb9444721f87abb1c9326f76d
Danilo Desole <danilodesole@gmail.com>
2020-01-23 09:24:30 +0000

    wip

--- s3/main.tf
+++ s3/main.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "b" {
-  bucket = "20200123-test-bucket"
+  bucket = "20200123-0924-test-bucket"
   acl    = "private"
 
   tags = {
-    Name        = "20200123-test-bucket"
+    Name        = "20200123-0924-test-bucket"
   }
 }
