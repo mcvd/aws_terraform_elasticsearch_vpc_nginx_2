2a1777142243618f00770f863877f2ea7e7d1cef
Danilo Desole <danilodesole@gmail.com>
2020-01-23 12:08:00 +0000

    wip

--- lambda/src/main.py
+++ lambda/src/main.py
@@ -1,12 +1,12 @@
 import boto3
 import re
 import requests
-from requests_aws4auth import AWS4Auth
+# from requests_aws4auth import AWS4Auth
 
 region = '' # e.g. us-west-1
 service = 'es'
 credentials = boto3.Session().get_credentials()
-awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+# awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
 
 host = '' # the Amazon ES domain, including https://
 index = 'lambda-s3-index'
